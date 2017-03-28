c3c5f32122406eb6512ca116a06032de6575785e
ZY <zhaoyueelf@163.com>
2017-03-28 20:08:08 +0800

    new

--- SQL/1-predeal.py
+++ SQL/1-predeal.py
@@ -1,11 +1,11 @@
 # predeal the user_data
+# 'user_id', 'age_left', 'age_right', 'sex', 'user_lv_cd', 'user_reg_dt'
 
 infile = open(r"D:\02\JData_User.csv", 'r')
-outfile = open(r"D:\02\1-data\user.csv", 'w')
+outfile = open(r"D:\02\1-data\user.txt", 'w')
 
 count = 0
 error = 0
-outfile.write(','.join(['user_id', 'age_left', 'age_right', 'sex', 'user_lv_cd', 'user_reg_dt']) + '\n')
 for line in infile.readlines():
     count += 1
     if count > 1:
@@ -21,16 +21,16 @@ for line in infile.readlines():
         else:
             error += 1
         outline = [line[0]] + age + line[2:]
-        outfile.write(','.join(outline) + '\n')
+        outfile.write('\t'.join(outline) + '\n')
 infile.close()
 outfile.close()
 
 
 # predeal the action_data
+# 'user_id,sku_id,time,model_id,type,cate,brand\n'
 
 period = ['_201602', '_201603', '_201603_extra', '_201604']
-outfile = open(r"D:\02\1-data\action.csv", 'w')
-outfile.write('user_id,sku_id,time,model_id,type,cate,brand\n')
+outfile = open(r"D:\02\1-data\action.txt", 'w')
 
 count = 0
 for time in period:
@@ -39,22 +39,30 @@ for time in period:
     line = infile.readline()
     while line:
         count += 1
-        outfile.write(line)
+        line = line.strip().split(',')
+        outfile.write('\t'.join(line) + '\n')
         line = infile.readline()
     infile.close()
 outfile.close()
 
 print "count: %d" % count
 
+
 # csv to txt
-'''
+
+import os
 files = ['JData_Product', 'JData_Comment']
 for file in files:
     infile = open("D:/02/%s.csv" % file, 'r')
     outfile = open('D:/02/1-data/%s.txt' % file, 'w')
-    for line in infile.readlines():
+    line = infile.readline()
+    line = infile.readline()
+    while line:
         line = line.strip().split(',')
-        outfile.write(','.join(line) + '\n')
+        outfile.write('\t'.join(line) + '\n')
+        line = infile.readline()
     infile.close()
     outfile.close()
-'''
+
+os.rename('D:/02/1-data/%s.txt' % 'JData_Product', 'D:/02/1-data/%s.txt' % 'product')
+os.rename('D:/02/1-data/%s.txt' % 'JData_Comment', 'D:/02/1-data/%s.txt' % 'comment')
